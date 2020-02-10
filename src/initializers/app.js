7aff9c1e3b67695cffc633718dd80fed9f0da4f8
renatodex <renatodex@gmail.com>
2020-02-10 01:38:50 -0300

    Add a way to control where to insert Canvas

--- src/initializers/app.js
+++ src/initializers/app.js
@@ -2,14 +2,16 @@ import Phaser from 'phaser'
 import { MainScene } from '@scenes/main_scene'
 
 export function AppInitializer ({
+  canvasElement,
   canvasWidth,
   canvasHeight,
-  gridSize
+  gridSize,
 }) {
   var game = new Phaser.Game({
-      type: Phaser.AUTO,
+      type: Phaser.WEBGL,
       width: canvasWidth,
       height: canvasHeight,
+      canvas: canvasElement,
       input: { queue: true },
   });
 
@@ -17,8 +19,9 @@ export function AppInitializer ({
     canvasWidth,
     canvasHeight,
     gridSize,
+    canvasElement,
     totalGridX: canvasWidth / gridSize,
-    totalGridY: canvasHeight / gridSize
+    totalGridY: canvasHeight / gridSize,
   }
 
   game.scene.add('MainScene', MainScene, true, {});
